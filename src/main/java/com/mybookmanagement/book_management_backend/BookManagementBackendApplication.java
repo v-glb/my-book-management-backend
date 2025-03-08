b45012710e4f893a0cdfcb8e81d5b2c405da27f0
Glebow <glebow@his.de>
2025-03-08 14:37:49 +0100

    Initial commit after setup with Spring Initializr

---
+++ src/main/java/com/mybookmanagement/book_management_backend/BookManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mybookmanagement.book_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mybookmanagement/book_management_backend/BookManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybookmanagement.book_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
