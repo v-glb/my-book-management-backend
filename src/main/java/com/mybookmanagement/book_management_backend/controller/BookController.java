6942f42e80933d31176cb5293bfb9fb81738f742
Glebow <glebow@his.de>
2025-03-09 20:41:29 +0100

    Add CORS config to allow request from frontend

--- src/main/java/com/mybookmanagement/book_management_backend/controller/BookController.java
+++ src/main/java/com/mybookmanagement/book_management_backend/controller/BookController.java
@@ -3,6 +3,7 @@ package com.mybookmanagement.book_management_backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.mybookmanagement.book_management_backend.repository.BookRepository;
 
 @RestController
 @RequestMapping("/api/books")
+@CrossOrigin(origins = "http://localhost:4200")
 public class BookController {
 
     @Autowired
