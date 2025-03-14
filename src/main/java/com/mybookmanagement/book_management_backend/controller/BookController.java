6eeb1f8130cfe4bfa6cc17ea812cb6408a1feb1d
Glebow <glebow@his.de>
2025-03-14 13:43:52 +0100

    Add mappings for updating and deleting books

--- src/main/java/com/mybookmanagement/book_management_backend/controller/BookController.java
+++ src/main/java/com/mybookmanagement/book_management_backend/controller/BookController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +41,29 @@ public class BookController {
         book.setCreatedAt(LocalDateTime.now());
         Book savedBook = bookRepository.save(book);
         return ResponseEntity.ok(savedBook);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        return bookRepository.findById(id)
+                .map(existingBook -> {
+                    existingBook.setTitle(book.getTitle());
+                    existingBook.setAuthor(book.getAuthor());
+                    existingBook.setRating(book.getRating());
+                    existingBook.setNotes(book.getNotes());
+                    Book updatedBook = bookRepository.save(existingBook);
+                    return ResponseEntity.ok(updatedBook);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}") 
+    public ResponseEntity<Object> deleteBook(@PathVariable Long id) {
+        return bookRepository.findById(id)
+                .map(book -> {
+                    bookRepository.delete(book);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
